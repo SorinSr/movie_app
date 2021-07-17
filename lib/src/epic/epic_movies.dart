8088b9a7e330028fc8d82bb294e30c4ae1d2e031
SorinSr <sorin.sr@gmail.com>
2021-07-17 19:27:43 +0300

    Add epic layer

--- lib/src/epic/epic_movies.dart
+++ lib/src/epic/epic_movies.dart
@@ -1,24 +1,27 @@
-// import 'package:redux_epics/redux_epics.dart';
-// import 'package:weather_app/src/actions/get_movies_actions.dart';
-// import 'package:weather_app/src/data/movie_api.dart';
-// import 'package:weather_app/src/model/app_state.dart';
-//
-// class AppEpics {
-//   const AppEpics({required MoviesApi moviesApi}) : _moviesApi = moviesApi;
-//
-//   final MoviesApi _moviesApi;
-//
-//   Epic<AppState> get epic {
-//     return combineEpics(<Epic<AppState>>[
-//       TypedEpic<AppState, GetMovies>(_getMovies),
-//     ],);
-//   }
-//
-//   Stream _getMovies(Stream <GetMovies> action, EpicStore<AppState> store) {
-//     return action
-//         .asyncMap((event) => _moviesApi.getMovies(store.state.page))
-//         .map<GetMoviesSuccessful>((movies) => GetMoviesSuccessful(movies));
-//
-//   }
-//
-// }
+import 'package:movies_app/src/actions/get_movies_actions.dart';
+import 'package:movies_app/src/data/movie_api.dart';
+import 'package:movies_app/src/model/app_state.dart';
+import 'package:movies_app/src/model/movie.dart';
+import 'package:redux_epics/redux_epics.dart';
+import 'package:rxdart/rxdart.dart';
+
+class AppEpics {
+  const AppEpics({required MoviesApi moviesApi}) : _moviesApi = moviesApi;
+
+  final MoviesApi _moviesApi;
+
+  Epic<AppState> get epic {
+    return combineEpics(
+      <Epic<AppState>>[
+        TypedEpic<AppState, GetMoviesActions>(_getMovies),
+      ],
+    );
+  }
+
+  Stream _getMovies(Stream<GetMoviesActions> action, EpicStore<AppState> store) {
+    return action
+        .asyncMap((GetMoviesActions action) => _moviesApi.getMovies(store.state.page))
+        .map<Object>((List<Movie> movies) => GetMoviesSuccessful(movies))
+        .onErrorReturnWith((Object error, StackTrace stackTrace) => GetMoviesError(error));
+  }
+}
