1e1049ba4140d39fdcf883f336c286ec56a443f0
SorinSr <sorin.sr@gmail.com>
2021-08-01 16:10:43 +0300

    Fix movie details page problems

--- lib/src/presentation/details_movie_page.dart
+++ lib/src/presentation/details_movie_page.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:movies_app/src/actions/index.dart';
 import 'package:movies_app/src/container/movie_details_container.dart';
-import 'package:movies_app/src/model/app_state.dart';
 import 'package:movies_app/src/model/movie.dart';
-import 'package:redux/redux.dart';
 
 class MovieDetails extends StatelessWidget {
   const MovieDetails({Key? key}) : super(key: key);
@@ -17,50 +13,43 @@ class MovieDetails extends StatelessWidget {
           appBar: AppBar(
             title: Text('Movie details:  \n' + movie.title),
           ),
-          body: GestureDetector(
-            onTap: () {
-              final Store<AppState> store = StoreProvider.of<AppState>(context);
-              final List<Movie> movies = store.state.movies.toList()..shuffle();
-              store.dispatch(SelectedMovie(movies.first.id));
-            },
-            child: SingleChildScrollView(
-              child: Card(
-                margin: const EdgeInsets.all(10),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(14.0),
-                ),
-                elevation: 24,
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(5, 15, 1, 5),
-                      child: Text(movie.title.toString()),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(movie.summary.toString(), textAlign: TextAlign.center),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: Text(movie.year.toString(), textAlign: TextAlign.center),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: Text(movie.genres.toString(), textAlign: TextAlign.center),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: Text(movie.rating.toString(), textAlign: TextAlign.center),
-                    ),
-                    Image.network(movie.largeCoverImage, width: 300, height: 450),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text('Go back!'),
-                    ),
-                  ],
-                ),
+          body: SingleChildScrollView(
+            child: Card(
+              margin: const EdgeInsets.all(10),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(14.0),
+              ),
+              elevation: 24,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(5, 15, 1, 5),
+                    child: Text(movie.title.toString()),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(movie.summary.toString(), textAlign: TextAlign.center),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Text(movie.year.toString(), textAlign: TextAlign.center),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Text(movie.genres.toString(), textAlign: TextAlign.center),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Text(movie.rating.toString(), textAlign: TextAlign.center),
+                  ),
+                  Image.network(movie.largeCoverImage, width: 300, height: 450),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Go back!'),
+                  ),
+                ],
               ),
             ),
           ),
