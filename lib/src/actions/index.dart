7525f13ddcca4cab5d6ca1869ba6815343281745
SorinSr <sorin.sr@gmail.com>
2021-08-05 14:46:44 +0300

    Formatting code

--- lib/src/actions/index.dart
+++ lib/src/actions/index.dart
@@ -1,28 +1,19 @@
 library actions;
 
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:movies_app/src/model/app_user.dart';
 import 'package:movies_app/src/model/movie.dart';
 import 'package:movies_app/src/model/review.dart';
-import 'package:movies_app/src/model/app_user.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:movies_app/src/actions/index.dart';
-
-part 'get_movies_freezed.dart';
-
-part 'register.dart';
-
-part 'sign_out.dart';
-
-part 'get_reviews.dart';
-
-part 'get_users.dart';
 
 part 'create_comment.dart';
-
-part 'init_app.dart';
-
-part 'selected_movie.dart';
-
+part 'get_movies_freezed.dart';
+part 'get_reviews.dart';
+part 'get_users.dart';
 part 'index.freezed.dart';
+part 'init_app.dart';
+part 'register.dart';
+part 'selected_movie.dart';
+part 'sign_out.dart';
 
 abstract class AppAction {}
 
